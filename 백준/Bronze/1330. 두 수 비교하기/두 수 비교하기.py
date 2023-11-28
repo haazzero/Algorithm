837fd14b1a245f121738c0be2c57c270ecbf5d3f
haazzero <125067896+haazzero@users.noreply.github.com>
2023-11-29 01:36:15 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -0,0 +1,12 @@
+A, B = input().split()
+if int(A) > int(B) :
+    print(">")
+elif int(A) < int(B) : 
+    print("<")
+else : 
+    print("==")
+
+# if문
+# if 조건 : 실행할 코드
+# elif 조건 : 실행할 코드
+# else : 실행할 코드
